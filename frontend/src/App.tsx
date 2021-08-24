5837825da2d5966323f01a5504b09986704bb8f9
lightcodes845 <dare.falola7@gmail.com>
2021-08-24 20:36:32 +0100

    added redirect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,13 @@ import { useActions } from "./hooks/useActions";
 function App() {
   const { authCheckState } = useActions();
 
-  if (window.location.host.startsWith("www")) {
-    window.location.href =
-      window.location.protocol +
-      "//" +
-      window.location.host +
-      window.location.pathname;
-  }
+  // if (window.location.host.startsWith("www")) {
+  //   window.location.href =
+  //     window.location.protocol +
+  //     "//" +
+  //     window.location.host +
+  //     window.location.pathname;
+  // }
 
   const checkAuth = useCallback(() => {
     authCheckState();
