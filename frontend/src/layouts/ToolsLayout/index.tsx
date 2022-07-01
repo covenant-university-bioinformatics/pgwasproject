b48d6ef4882d30fb7dfd0e9c90a4c48a58f65fbe
lightcodes845 <dare.falola7@gmail.com>
2022-07-01 18:05:24 +0100

    scroll to top on navigation

--- frontend/src/layouts/ToolsLayout/index.tsx
+++ frontend/src/layouts/ToolsLayout/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import pgwasAxios from "../../axios-fetches";
 import MainLayout from "../MainLayout";
 import classes from "./index.module.scss";
@@ -23,6 +23,11 @@ type Props = {
 const ToolsLayout: React.FC<Props> = ({ title, path, children }) => {
   const { user } = useTypedSelector((state) => state.auth);
   const [showButton, setShowButton] = useState(true);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const resendConfirm = () => {
     pgwasAxios
       .get("/auth/resendconfirm")
